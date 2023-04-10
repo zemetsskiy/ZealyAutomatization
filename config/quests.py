c448868982945453c06667495d7e163a67cd7234
zemetsskiy <yaroslavzhemerikin_@outlook.com>
2023-04-10 13:52:49 +0300

    config folder added, quests info added

---
+++ config/quests.py
@@ -0,0 +1,79 @@
+# quests = {
+#    category_id: {
+#        quest_id: answer,
+#    },
+# }
+
+
+# categories: Special Gift, Invites are skipped.
+quests = {
+    "f5161d37-8b9c-4eee-9389-72d62234e4e4": "b7c3412f-c534-45dc-baf7-fbaee7fdd085", # onboarding: 1 quest (just claim)
+    "61688e80-62a9-4e22-8b43-8e7e95b2df2e": { # special
+        "9b5d7b91-c7b9-4b8c-8506-d0623b41f1be": "Web3Space_5670204144635964",
+        "b73771b6-f642-4410-afba-1b67a164b598": "SuiGlobal_59d879cc",
+        "cb6dfe28-0827-4af1-8370-cd8045baf2b7": "SuiEcosystem_11ed",
+    },
+    "2db9007a-eaa3-4ac1-a948-0ad7e40d0669": { # join (just claim)
+        "834e8702-773a-4ea5-82cd-0b8987ad8cb9", "afddae52-4e18-4a96-97db-8c82385a1ae7", "14237ce9-121f-4288-9c2a-e596987151cf", "swap_on_suiswap???", "5b28150f-4ee6-4e03-9607-68040987628e"
+    },
+    "698e2a4a-352a-4642-aa78-21cccee6cdc8": { # boost (just claim)
+        "e3073a79-0ebc-465c-9e2c-7f4eeecebb0b", "1d9824e7-b2d9-4235-839d-20f7f7aa282a", "c4fb82be-d091-45b4-b4d8-bb3915563b57", "9df2617f-fcb2-4c0b-87bb-c24edeeb7697" # last 2 are lvl8+
+    },
+    "b4de062b-5143-4604-88f8-044161969fb2": { # quiz
+        "192a1798-70a9-4939-9da2-73ed0374451d": "A unified swapping platform built on SUI",
+        "fb55fe60-0ae5-485c-bdec-1f66004248de": "2022.9",
+        "ed4254cc-9ecd-41c5-aa49-46be5ecbfa0d": "A community point that can be redeemed for airdrop",
+        "622fc736-a524-4692-9620-bf00aae9eaab": "Exchange for Suiswap token after mainnet launch",
+        "a801e865-dc48-478b-a784-2e963510f7e3": "Yes",
+        "956aa052-1042-4073-871c-982bf53b9746": "Yes",
+        "6a120dfe-4e01-4fda-a1c6-d1dc9d740546": "Yes",
+        "0d3fccc4-f4f4-4785-aa60-0826382f774d": "Yes",
+        "ee561f58-8be3-4853-9faf-46212c2a8a4a": "Weighted by the Suiswap Points you have",
+        "bf6bf666-d5fe-4836-be5b-cd772fc2ac9a": "Yes, it will be automatically converted",
+        "2b034bc3-f0ac-407d-a635-102a74655309": "Yes, it will be automatically converted",
+        "6ae7a9b8-df63-49a6-bdad-470da36aa3c8": "$SSWP",
+        "c887aa09-45e8-44f3-b797-b39f16cc38e8": "Yes, you can stake the Suiswap token to earn platform rewards",
+        "fc2fac90-f886-41f3-9c8e-a7bd92679f66": "suiswap.app",
+        "2d965df8-52ed-44d6-9604-eb747c6ebaa1": "X * Y = K ( Constant Product)",
+        "9bbfdf96-1935-4e55-bb40-8c1a479d8b92": "It allows you to trade with lower slippage",
+        "a13f7af5-86e2-4453-a482-9823bc212f59": "Ethos Wallet"
+    },
+    "cdd789f2-df3b-4498-b75a-ecdfe31731cd": { # twitter
+        "6af63638-6c87-4caf-bade-a08ec1b42896": "", # tweet_link
+        "31876fc2-948e-4c04-8936-2f25a152c20e": "", # tweet_link
+        "dbde2f07-5fd4-47f2-8082-a69557838633": "", # tweet_link
+        "a31cc2c6-8891-4118-b46d-2571ab357c21": "", # claim
+        "dbd40e8b-dc57-4685-8875-5282c5b458f6": "", # claim
+        "ec52e174-437d-40f7-9761-ea865e8dab5b": "", # claim
+        "2e97b94f-a3e8-454c-b0a6-11880b6028db": "", # claim
+        "4c282823-9c05-4821-9833-192196556b92": "", # claim
+     },
+    "18f19041-8751-4f43-940f-85770f61e834": { # partner twitter follow (just claim)
+        "81ac3145-1c92-4006-a142-67e10a66d4df",
+        "c7baab49-b7f7-4bc0-8da3-b941584ddc41",
+        "ac4d81cf-c75f-44b7-911a-53b6f2f1891f",
+        "49c99964-c45a-46e1-9059-f64aa3e1c238",
+        "a8105444-ee10-4661-a301-cb9d679e116e",
+        "61e0a852-d3e2-46ae-b597-41c023072514",
+        "7ccbbc38-2578-447c-9429-0e00fc955807",
+        "767eb4a1-7679-4cb5-b60e-893c4a3dcb6a",
+        "86fba347-5cb0-4e80-81a5-8a16257295b7",
+        "0dbcd368-4395-404a-b5dc-c5023bde1202",
+        "6f77ce4d-22d5-4167-8064-cafad0462904"
+    },
+    "d833e1f4-251e-4859-828d-afa5c7abd34d": { # suiswap friend follow
+        "273df3e9-eb21-4c70-9de2-a2d1e52a7552",
+        "ee1e3c93-3cf7-4a87-8af7-8d427d4b87f6",
+        "7e421135-2451-4df5-92c3-646b1e8a9f6a",
+        "c3a86e75-2646-4c47-bacd-2f1e74290db3",
+        "f484f0be-bdb8-4a27-91e8-9dcb2fe5ce53",
+        "93338dae-0b3e-4e1e-91de-b382d10c72ed",
+        "904cca49-d5ed-4c80-bfad-0a123e3619e3",
+        "aa77659a-8c8c-4352-94a5-d1fe0490a6f9",
+        "3c183088-6681-45c5-800c-568d3260b6e4",
+        "af2a5017-bf63-4533-a7f2-1a66ea9aafb5",
+        "96a34359-de24-47c2-b8e2-72de94f8a559",
+        "5c57942c-f359-4cf0-a59d-fb3a08ae6ab8"
+    }
+
+}
