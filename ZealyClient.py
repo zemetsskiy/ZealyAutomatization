f452add38d76b322568d7387a14060a70be080be
zemetsskiy <yaroslavzhemerikin_@outlook.com>
2023-04-10 19:23:53 +0300

    updated

--- ZealyClient.py
+++ ZealyClient.py
@@ -5,3 +5,27 @@ class ZealyClient:
     # claim link ex: https://api.zealy.io/communities/suiswap-app/quests/b7c3412f-c534-45dc-baf7-fbaee7fdd085/claim
     def __init__(self):
         self.base_url = "https://api.zealy.io/communities/suiswap-app/quests"
+
+    def claim_onboarding(self):
+        pass
+
+    def claim_special(self):
+        pass
+
+    def claim_join(self):
+        pass
+
+    def claim_boost(self):
+        pass
+
+    def claim_quiz(self):
+        pass
+
+    def claim_twitter(self):
+        pass
+
+    def claim_partner_twitter(self):
+        pass
+
+    def claim_suiswap_friend(self):
+        pass
