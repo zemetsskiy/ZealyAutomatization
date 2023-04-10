ca67b6869f4a742c6f0006e3427d594201f3f9e0
zemetsskiy <yaroslavzhemerikin_@outlook.com>
2023-04-10 14:54:47 +0300

    zealyclient class created

---
+++ ZealyClient.py
@@ -0,0 +1,7 @@
+import aiohttp
+
+
+class ZealyClient:
+    # claim link ex: https://api.zealy.io/communities/suiswap-app/quests/b7c3412f-c534-45dc-baf7-fbaee7fdd085/claim
+    def __init__(self):
+        self.base_url = "https://api.zealy.io/communities/suiswap-app/quests"
