61af6812c8e9c0a51ae4033cb6ae95eaf8ff500f
zemetsskiy <yaroslavzhemerikin_@outlook.com>
2023-04-12 11:39:41 +0300

    claim_twitter func added

--- ZealyClient.py
+++ ZealyClient.py
@@ -1,9 +1,9 @@
 import asyncio
 import aiohttp
-import requests
 
 from config.quests import quests
 from config.params import cookies, get_header, get_quest_url
+from config.tweet_links import tweet_links
 
 
 async def send_post_request(session, url, data, headers):
@@ -50,8 +50,21 @@ class ZealyClient:
                     result = await send_post_request(session, get_quest_url(key), data, get_header(data[2:40]))
                     print(f'Response {key[0:4]}: {result}')
 
-    def claim_twitter(self):
-        pass
+    @staticmethod
+    async def claim_twitter():
+        async with aiohttp.ClientSession() as session:
+            for key, data in quests["twitter"].items():
+                if data != "":
+                    result = await send_post_request(session, get_quest_url(key), data, get_header(data[2:40]))
+                    print(f'Response {key[0:4]}: {result}')
+                elif key == "6af63638-6c87-4caf-bade-a08ec1b42896":
+                    boundary = '------WebKitFormBoundaryyrn9YrjQm5U5rTnW\r\nContent-Disposition: form-data; name="value"\r\n\r\n'+f'{tweet_links["spread"]}'+'\r\n------WebKitFormBoundaryyrn9YrjQm5U5rTnW\r\nContent-Disposition: form-data; name="questId"\r\n\r\n6af63638-6c87-4caf-bade-a08ec1b42896\r\n------WebKitFormBoundaryyrn9YrjQm5U5rTnW\r\nContent-Disposition: form-data; name="type"\r\n\r\ntwitter\r\n------WebKitFormBoundaryyrn9YrjQm5U5rTnW--\r\n'
+                    result = await send_post_request(session, get_quest_url(key), data, get_header(boundary[2:40]))
+                    print(f'Response {key[0:4]}: {result}')
+                #elif key == "31876fc2-948e-4c04-8936-2f25a152c20e":
+                #    boundary =
+                #    result = await send_post_request(session, get_quest_url(key), data, get_header(boundary[2:40]))
+                #    print(f'Response {key[0:4]}: {result}')
 
     @staticmethod
     async def claim_partner_twitter():
@@ -72,7 +85,7 @@ class ZealyClient:
 
 def main():
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(ZealyClient.claim_join())
+    loop.run_until_complete(ZealyClient.claim_twitter())
 
 
 if __name__ == "__main__":
