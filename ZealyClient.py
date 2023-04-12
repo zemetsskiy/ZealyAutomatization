92f81e4e0c680e09a0cf9e4166b8136d657c20e7
artemtret <artem.tretykov74@gmail.com>
2023-04-12 23:57:03 +0300

    add move routes

--- ZealyClient.py
+++ ZealyClient.py
@@ -3,40 +3,11 @@ import time
 import aiohttp
 
 from config.quests import quests
-from config.params import get_cookies, get_header, get_quest_url, get_url_proxies, get_auth_proxies, profile_link, \
-    GET_header
+from config.params import get_cookies, get_header, get_quest_url,  profile_link, GET_header
 from config.tokens import token_to_proxies
 from config.logger import logger
 from config.tweet_links import tweet_links
-
-
-async def send_post_request(session, url, data, headers, cookies, proxy):
-    proxy_url = get_url_proxies(proxy)
-    username = get_auth_proxies(proxy)[0]
-    password = get_auth_proxies(proxy)[1]
-    proxy_auth = aiohttp.BasicAuth(username, password)
-
-    async with session.post(url, data=data, headers=headers, cookies=cookies, proxy=proxy_url,
-                            proxy_auth=proxy_auth) as resp:
-        warn = ""
-        data = await resp.json()
-        if "error" in data:
-            warn = data['error']['follow']
-        elif "message" in data:
-            warn = data['message'] + " or Already Claimed"
-        return resp.status, warn
-
-
-async def send_get_request(session, url, headers, cookies):
-    async with session.get(url, headers=headers, cookies=cookies) as resp:
-        response = await resp.json()
-        return response
-
-
-async def get_me(access_token):
-    async with aiohttp.ClientSession() as session:
-        result = await send_get_request(session, profile_link, GET_header, get_cookies(access_token))
-        return result["discordHandle"]
+from routes import send_post_request, send_get_request, get_me
 
 
 class ZealyClient:
