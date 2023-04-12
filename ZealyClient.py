0281de774b78681560d9ed2a7d62ec67a03f9ee4
zemetsskiy <yaroslavzhemerikin_@outlook.com>
2023-04-12 20:23:58 +0300

    methods updated

--- ZealyClient.py
+++ ZealyClient.py
@@ -7,6 +7,7 @@ from config.params import get_cookies, get_header, get_quest_url, get_url_proxie
     GET_header
 from config.tokens import token_to_proxies
 from config.logger import logger
+from config.tweet_links import tweet_links
 
 
 async def send_post_request(session, url, data, headers, cookies, proxy):
@@ -33,12 +34,28 @@ class ZealyClient:
     def __init__(self):
         self.base_url = "https://api.zealy.io/communities/suiswap-app/quests"
 
+
     @staticmethod
     async def claim_onboarding():
-        async with aiohttp.ClientSession() as session:
-            for key, data in quests["onboarding"].items():
-                result = await send_post_request(session, get_quest_url(key), data, get_header(data[2:40]))
-                print(f'Response {key[0:4]}: {result}')
+        for acc_token, acc_proxy in token_to_proxies.items():
+            async with aiohttp.ClientSession() as session:
+                try:
+                    for key, data in quests["onboarding"].items():
+                        resp = await send_post_request(session,
+                                                       get_quest_url(key),
+                                                       data,
+                                                       get_header(data[2:40]),
+                                                       get_cookies(acc_token),
+                                                       acc_proxy)
+                        if resp == 200:
+                            logger.info(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- SUCCESS')
+                        elif resp == 400:
+                            logger.warning(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- QUEST ALREADY CLAIMED')
+                        else:
+                            logger.error(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- SOMETHING WENT WRONG')
+                        time.sleep(2)
+                except aiohttp.ClientError as err_:
+                    logger.error(f"ERROR: {err_}")
 
     @staticmethod
     async def claim_special():
@@ -55,67 +72,163 @@ class ZealyClient:
                         if resp == 200:
                             logger.info(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- SUCCESS')
                         elif resp == 400:
-                            logger.warning(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- QUESTS ALREADY CLAIMED')
+                            logger.warning(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- QUEST ALREADY CLAIMED')
                         else:
                             logger.error(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- SOMETHING WENT WRONG')
                         time.sleep(2)
                 except aiohttp.ClientError as err_:
                     logger.error(f"ERROR: {err_}")
 
-    # @staticmethod
-    # async def claim_quiz():
-    #     async with aiohttp.ClientSession() as session:
-    #         for key, data in quests["quiz"].items():
-    #             result = await send_post_request(session, get_quest_url(key), data, get_header(data[2:40]))
-    #             print(f'Response {key[0:4]}: {result}')
+    @staticmethod
+    async def claim_quiz():
+        for acc_token, acc_proxy in token_to_proxies.items():
+            async with aiohttp.ClientSession() as session:
+                try:
+                    for key, data in quests["quiz"].items():
+                        resp = await send_post_request(session,
+                                                       get_quest_url(key),
+                                                       data,
+                                                       get_header(data[2:40]),
+                                                       get_cookies(acc_token),
+                                                       acc_proxy)
+                        if resp == 200:
+                            logger.info(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- SUCCESS')
+                        elif resp == 400:
+                            logger.warning(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- QUEST ALREADY CLAIMED')
+                        else:
+                            logger.error(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- SOMETHING WENT WRONG')
+                        time.sleep(2)
+                except aiohttp.ClientError as err_:
+                    logger.error(f"ERROR: {err_}")
 
     @staticmethod
     async def claim_boost():
-        async with aiohttp.ClientSession() as session:
-            for key, data in quests["boost"].items():
-                if data != "":
-                    result = await send_post_request(session, get_quest_url(key), data, get_header(data[2:40]))
-                    print(f'Response {key[0:4]}: {result}')
+        for acc_token, acc_proxy in token_to_proxies.items():
+            async with aiohttp.ClientSession() as session:
+                try:
+                    for key, data in quests["boost"].items():
+                        if data != "":
+                            resp = await send_post_request(session,
+                                                           get_quest_url(key),
+                                                           data,
+                                                           get_header(data[2:40]),
+                                                           get_cookies(acc_token),
+                                                           acc_proxy)
+                            if resp == 200:
+                                logger.info(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- SUCCESS')
+                            elif resp == 400:
+                                logger.warning(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- QUEST ALREADY CLAIMED')
+                            else:
+                                logger.error(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- SOMETHING WENT WRONG')
+                            time.sleep(2)
+                except aiohttp.ClientError as err_:
+                    logger.error(f"ERROR: {err_}")
 
     @staticmethod
     async def claim_join():
-        async with aiohttp.ClientSession() as session:
-            for key, data in quests["join"].items():
-                if not key in {"14237ce9-121f-4288-9c2a-e596987151cf", "08ebac1b-18e4-4807-80c2-b70fa1882cd9"}:
-                    result = await send_post_request(session, get_quest_url(key), data, get_header(data[2:40]))
-                    print(f'Response {key[0:4]}: {result}')
+        for acc_token, acc_proxy in token_to_proxies.items():
+            async with aiohttp.ClientSession() as session:
+                try:
+                    for key, data in quests["join"].items():
+                        if not key in {"14237ce9-121f-4288-9c2a-e596987151cf", "08ebac1b-18e4-4807-80c2-b70fa1882cd9"}:
+                            resp = await send_post_request(session,
+                                                           get_quest_url(key),
+                                                           data,
+                                                           get_header(data[2:40]),
+                                                           get_cookies(acc_token),
+                                                           acc_proxy)
+                            if resp == 200:
+                                logger.info(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- SUCCESS')
+                            elif resp == 400:
+                                logger.warning(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- QUEST ALREADY CLAIMED')
+                            else:
+                                logger.error(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- SOMETHING WENT WRONG')
+                            time.sleep(2)
+                except aiohttp.ClientError as err_:
+                    logger.error(f"ERROR: {err_}")
 
     @staticmethod
     async def claim_twitter():
-        async with aiohttp.ClientSession() as session:
-            for key, data in quests["twitter"].items():
-                if data != "":
-                    result = await send_post_request(session, get_quest_url(key), data, get_header(data[2:40]))
-                    print(f'Response {key[0:4]}: {result}')
-                elif key == "6af63638-6c87-4caf-bade-a08ec1b42896":
-                    boundary = '------WebKitFormBoundaryyrn9YrjQm5U5rTnW\r\nContent-Disposition: form-data; name="value"\r\n\r\n'+f'{tweet_links["spread"]}'+'\r\n------WebKitFormBoundaryyrn9YrjQm5U5rTnW\r\nContent-Disposition: form-data; name="questId"\r\n\r\n6af63638-6c87-4caf-bade-a08ec1b42896\r\n------WebKitFormBoundaryyrn9YrjQm5U5rTnW\r\nContent-Disposition: form-data; name="type"\r\n\r\ntwitter\r\n------WebKitFormBoundaryyrn9YrjQm5U5rTnW--\r\n'
-                    result = await send_post_request(session, get_quest_url(key), data, get_header(boundary[2:40]))
-                    print(f'Response {key[0:4]}: {result}')
-                #elif key == "31876fc2-948e-4c04-8936-2f25a152c20e":
-                #    boundary =
-                #    result = await send_post_request(session, get_quest_url(key), data, get_header(boundary[2:40]))
-                #    print(f'Response {key[0:4]}: {result}')
+        for acc_token, acc_proxy in token_to_proxies.items():
+            async with aiohttp.ClientSession() as session:
+                try:
+                    for key, data in quests["twitter"].items():
+                        if data != "":
+                            resp = await send_post_request(session,
+                                                           get_quest_url(key),
+                                                           data,
+                                                           get_header(data[2:40]),
+                                                           get_cookies(acc_token),
+                                                           acc_proxy)
+                            if resp == 200:
+                                logger.info(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- SUCCESS')
+                            elif resp == 400:
+                                logger.warning(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- QUEST ALREADY CLAIMED')
+                            else:
+                                logger.error(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- SOMETHING WENT WRONG')
+                            time.sleep(2)
+                        elif key == "6af63638-6c87-4caf-bade-a08ec1b42896":
+                            boundary = '------WebKitFormBoundaryyrn9YrjQm5U5rTnW\r\nContent-Disposition: form-data; name="value"\r\n\r\n' + f'{tweet_links["spread"]}' + '\r\n------WebKitFormBoundaryyrn9YrjQm5U5rTnW\r\nContent-Disposition: form-data; name="questId"\r\n\r\n6af63638-6c87-4caf-bade-a08ec1b42896\r\n------WebKitFormBoundaryyrn9YrjQm5U5rTnW\r\nContent-Disposition: form-data; name="type"\r\n\r\ntwitter\r\n------WebKitFormBoundaryyrn9YrjQm5U5rTnW--\r\n'
+                            resp = await send_post_request(session,
+                                                           get_quest_url(key),
+                                                           data,
+                                                           get_header(boundary[2:40]),
+                                                           get_cookies(acc_token),
+                                                           acc_proxy)
+                            if resp == 200:
+                                logger.info(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- SUCCESS')
+                            elif resp == 400:
+                                logger.warning(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- QUEST ALREADY CLAIMED')
+                            else:
+                                logger.error(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- SOMETHING WENT WRONG')
+                            time.sleep(2)
 
-    # @staticmethod
-    # async def claim_partner_twitter():
-    #     async with aiohttp.ClientSession() as session:
-    #         for key, data in quests["partner_twitter"].items():
-    #             result = await send_post_request(session, get_quest_url(key), data, get_header(data[2:40]))
-    #             print(f'Response {key[0:4]}: {result}')
+                except aiohttp.ClientError as err_:
+                    logger.error(f"ERROR: {err_}")
 
-    # @staticmethod
-    # async def claim_suiswap_friend():
-    #     async with aiohttp.ClientSession() as session:
-    #         for key, data in quests["suiswap_friend"].items():
-    #             result = await send_post_request(session, get_quest_url(key), data, get_header(data[2:40]))
-    #             print(f'Response {key[0:4]}: {result}')
+    @staticmethod
+    async def claim_partner_twitter():
+        for acc_token, acc_proxy in token_to_proxies.items():
+            async with aiohttp.ClientSession() as session:
+                try:
+                    for key, data in quests["partner_twitter"].items():
+                        resp = await send_post_request(session,
+                                                       get_quest_url(key),
+                                                       data,
+                                                       get_header(data[2:40]),
+                                                       get_cookies(acc_token),
+                                                       acc_proxy)
+                        if resp == 200:
+                            logger.info(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- SUCCESS')
+                        elif resp == 400:
+                            logger.warning(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- QUEST ALREADY CLAIMED')
+                        else:
+                            logger.error(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- SOMETHING WENT WRONG')
+                        time.sleep(2)
+                except aiohttp.ClientError as err_:
+                    logger.error(f"ERROR: {err_}")
 
-    # TODO Получать XP каждого клиента
+    @staticmethod
+    async def claim_suiswap_friend():
+        for acc_token, acc_proxy in token_to_proxies.items():
+            async with aiohttp.ClientSession() as session:
+                try:
+                    for key, data in quests["suiswap_friend"].items():
+                        resp = await send_post_request(session,
+                                                       get_quest_url(key),
+                                                       data,
+                                                       get_header(data[2:40]),
+                                                       get_cookies(acc_token),
+                                                       acc_proxy)
+                        if resp == 200:
+                            logger.info(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- SUCCESS')
+                        elif resp == 400:
+                            logger.warning(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- QUEST ALREADY CLAIMED')
+                        else:
+                            logger.error(f'RESPONSE {key[0:4]}: STATUS CODE {resp} --- SOMETHING WENT WRONG')
+                        time.sleep(2)
+                except aiohttp.ClientError as err_:
+                    logger.error(f"ERROR: {err_}")
 
     @staticmethod
     async def get_xp(access_token):
